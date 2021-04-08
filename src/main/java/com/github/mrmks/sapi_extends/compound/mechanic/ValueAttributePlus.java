67c4aa6258cff2390deac05048a8aca73db95fcd
MrMks <i0991958@163.com>
2021-04-08 23:57:46 +0800

    0.0.2-8 ValueAttributePlus

--- src/main/java/com/github/mrmks/sapi_extends/compound/mechanic/ValueAttributePlus.java
+++ src/main/java/com/github/mrmks/sapi_extends/compound/mechanic/ValueAttributePlus.java
@@ -23,7 +23,7 @@ public class ValueAttributePlus extends CustomMechanic {
 
     @Override
     public String getKey() {
-        return "value_attribute_plus";
+        return "value_attributePlus";
     }
 
     public String getDisplayName() {
@@ -38,7 +38,7 @@ public class ValueAttributePlus extends CustomMechanic {
     @Override
     public List<EditorOption> getOptions() {
         return ImmutableList.of(
-                EditorOption.text(ATTR_KEY, "AttrKey", "[" + ATTR_KEY + "]The name of the attribute used in AttributePlus, please DO NOT append with \"[0] or \"[1]", ""),
+                EditorOption.text(ATTR_KEY, "AttrKey", "[" + ATTR_KEY + "]The name of the attribute used in AttributePlus, please DO NOT append with \\\"[0]\\\" or \\\"[1]\\\"", ""),
                 EditorOption.dropdown(MIN_MAX, "Min_Max", "["+MIN_MAX+"]Select min or max value will be stored", ImmutableList.of("min", "max")),
                 EditorOption.dropdown(RANDOM, "Random", "["+RANDOM+"]Generate a random value between min and max? Option Min_Max will be ignored if set this to true", ImmutableList.of("false", "true")),
                 EditorOption.text(KEY, "Key", "["+KEY+"]The unique key to store the value under", "key")
@@ -66,6 +66,7 @@ public class ValueAttributePlus extends CustomMechanic {
         if (random) {
             double min = doubles.length > 0 ? doubles[0].doubleValue() : 0;
             double max = doubles.length > 1 ? doubles[1].doubleValue() : 0;
+            if (max < min) max = min;
             res = min + Rand.nextDouble() * (max - min);
         } else {
             int index = minMax ? 0 : 1;
